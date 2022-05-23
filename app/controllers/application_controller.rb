f0664b0ceb3a6856585f7afbf694cbc5a19ee341
Mark McDermott <mark@markmcdermott.io>
2022-05-23 14:09:11 -0500

    add admin functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :authorized
+  helper_method :current_user_is_admin?
   helper_method :current_user 
   helper_method :logged_in?
   helper_method :current_user_id_class
@@ -9,6 +10,10 @@ class ApplicationController < ActionController::Base
   def authorized
     puts 'checking authorization status: ' + logged_in?.to_s
     redirect_to '/' unless logged_in?
+  end
+
+  def current_user_is_admin?
+    current_user.admin
   end
 
   def current_user
