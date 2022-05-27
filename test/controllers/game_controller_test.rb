ecb74462e2d5a9235ca83910380c4c8f07513c96
Mark McDermott <mark@markmcdermott.io>
2022-05-27 14:23:30 -0500

    run game controller test on circleci

--- test/controllers/game_controller_test.rb
+++ test/controllers/game_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class GameControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get game_index_url
+    get '/'
     assert_response :success
   end
 end
